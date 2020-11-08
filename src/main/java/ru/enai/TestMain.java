87ae3f78001cb3f806e8efff82d44fd963cd5dac
Enai63 <Toha76@yandex.ru>
2020-11-08 21:53:34 +0400

    ten learn

--- src/main/java/ru/enai/TestMain.java
+++ src/main/java/ru/enai/TestMain.java
@@ -5,11 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appliactionContext.xml");
-//        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("rockMusic", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 //        musicPlayer.playMusic();
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getName() + " " + musicPlayer.getVolume());
+
         context.close();
     }
 }
